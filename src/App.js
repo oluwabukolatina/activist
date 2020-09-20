533973a5374cfb6827b04c87e0e688294821980d
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-09-20 11:19:29 +0100

    lint-staged

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 
-console.log('hello')
-
 function App() {
   return (
     <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
   );
 }
 
