cc78f01967f81aeea4e9aabdf9e47efcf185a500
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-09-20 13:29:19 +0100

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+
 const Home = () => {
-         return (
-                  <div>home</div>
-         )
-}
-export default Home
+  return (
+    <div className="app">
+      <div className="logo">ActivistHub</div>
+      <div className="cta">
+        <p>Learn About Historical Figures Who</p>
+        <p>Have Made Meaningful Social Change.</p>
+        <button>View Activists</button>
+      </div>
+    </div>
+  );
+};
+export default Home;
