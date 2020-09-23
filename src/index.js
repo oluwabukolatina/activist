673199b8e12426b2031e9de85ef57d64305a78c6
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-09-23 23:36:27 +0100

    rm netlify script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
