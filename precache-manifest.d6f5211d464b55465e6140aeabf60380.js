0370c70e2539b447b2914c10bcc0183ecf2fad62
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-09-23 23:34:31 +0100

    Updates

---
+++ precache-manifest.d6f5211d464b55465e6140aeabf60380.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3689c1efa11db0eb1d53d607eb0c32",
+    "url": "/activist/index.html"
+  },
+  {
+    "revision": "c84e6a90b8de22c77576",
+    "url": "/activist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f5989aaef76107a0633",
+    "url": "/activist/static/css/main.7e1814a4.chunk.css"
+  },
+  {
+    "revision": "c84e6a90b8de22c77576",
+    "url": "/activist/static/js/2.75de30c1.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/activist/static/js/2.75de30c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5989aaef76107a0633",
+    "url": "/activist/static/js/main.149a923f.chunk.js"
+  },
+  {
+    "revision": "12e9721357139401e2b1",
+    "url": "/activist/static/js/runtime-main.0d23377d.js"
+  },
+  {
+    "revision": "79e8c4a1eb7c12758164c118bfa463b0",
+    "url": "/activist/static/media/teemu-paananen-rd5uNIUJCF0-unsplash 1.79e8c4a1.jpg"
+  }
+]);
